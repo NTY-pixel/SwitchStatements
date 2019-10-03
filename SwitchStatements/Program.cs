32f36825106814c99ccb54f77b6199003bfa02e1
NTY-pixel <5211859@student.gloscol.ac.uk>
2019-10-03 10:15:19 +0100

    More code

--- SwitchStatements/Program.cs
+++ SwitchStatements/Program.cs
@@ -6,7 +6,17 @@ namespace SwitchStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string sFullName;
+            string sgreeting;
+
+            //get name from user 
+            Console.WriteLine("Please enter full name");
+            sFullName = console.ReadLine();
+            //check name
+            switch (sFullName);
+            {
+                case "Bill Gates";
+            }
         }
     }
 }
